1f2f42ef26c15d73dd7babc9b11725697e9f4d2a
jackgit <ygjack414@hotmail.com>
2017-06-05 10:08:16 +0800

    removedisc

--- config/index.js
+++ config/index.js
@@ -4,8 +4,8 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../disc/index.html'),
-    assetsRoot: path.resolve(__dirname, '../disc'),
+    index: path.resolve(__dirname, '../dist/index.html'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: 'http://qiniu.jackyang.me/xplan/',    // /
     productionSourceMap: true,
